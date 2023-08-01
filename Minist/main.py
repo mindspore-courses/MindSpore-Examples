429e713511adb3500b0ce71fbc7994ccccbbcd97
Menghuan1918 <Menghuan2003@outlook.com>
2023-08-01 22:08:09 +0800

    完成

--- Minist/main.py
+++ Minist/main.py
@@ -37,22 +37,25 @@ def get_parser():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    #默认值是14!!!记得改回去!!!!!!!!!!!!!!!!!
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                        help='number of epochs to train (default: 2)')
+    #因为内存占用问题从参考项目的14改为2,但是准确度变化不大。
+    #Changed from 14 to 2 for the reference project because of memory usage issues, but accuracy did not change much.
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-gpu', action='store_true', default=False,
                         help='disables GPU training')
+    #minspore没有对苹果GPU的支持，此处删除一个指令
+    #minspore has no support for Apple GPUs, remove a directive here
     parser.add_argument('--dry-run', action='store_true', default=False,
                         help='quickly check a single pass')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=50, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=True,
+    parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
     return args
@@ -63,7 +66,8 @@ def learning_rate_function(lr, cur_step_num):
     return lr 
 
 def train(args, model, train_loader):
-    model.train(epoch=args.epochs, train_dataset=train_loader, callbacks=[LearningRateScheduler(learning_rate_function),LossMonitor(0.01,1875)])
+    model.train(epoch=args.epochs, train_dataset=train_loader, 
+                callbacks=[LearningRateScheduler(learning_rate_function),LossMonitor(0.01,args.log_interval)])
      
 
 def test(model,test_loader):
@@ -75,15 +79,19 @@ def main(args):
     if args.no_gpu:
         ms.set_context(device_target="CPU")
     ms.set_seed(args.seed)
+    if args.dry_run:
+        args.epochs = 1
 
     # Load data
-    download_train = Mnist(path="../data/mnist", split="train", batch_size=32, repeat_num=1, shuffle=True, resize=32, download=True)
+    download_train = Mnist(path="../data/mnist", split="train", batch_size=args.batch_size, 
+                           repeat_num=1, shuffle=True, resize=32, download=True)
     download_eval = Mnist(path="../data/mnist", split="test", batch_size=32, resize=32, download=True)
     dataset1 = download_train.run() #Train
     dataset2 = download_eval.run() #Test
     model = Net()
 
-    optimizer =  nn.optim.Adadelta(model.trainable_params(), learning_rate=args.lr, rho=0.9, weight_decay=0.0)
+    optimizer =  nn.optim.Adadelta(model.trainable_params(), learning_rate=args.lr, 
+                                   rho=0.9, weight_decay=0.0)
     criterion = nn.NLLLoss()
     train_model = Model(network=model, loss_fn=criterion, optimizer=optimizer,metrics={'accuracy'})
 
